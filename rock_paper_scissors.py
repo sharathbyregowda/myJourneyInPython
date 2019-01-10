3e84a0677ee2bb185b465d126a98b8c1534dac51
sharathbyregowda <sharu.b@gmail.com>
2019-01-10 17:10:34 +0000

    initializing score variables inside method

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -54,13 +54,12 @@ class CyclerPlayer(Player):
 
 class Game:
 
-    player1 = 0
-    player2 = 0
-    draw = 0
-
     def __init__(self, p1, p2):
         self.p1 = p1
         self.p2 = p2
+        self.player1 = 0
+        self.player2 = 0
+        self.draw = 0
 
     def play_round(self):
         move1 = self.p1.move()
