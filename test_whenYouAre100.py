a3d7d80cbb8678168054049a9e95891a6f25409b
sharathbyregowda <sharu.b@gmail.com>
2019-02-19 23:42:20 +0000

    Create test_whenYouAre100.py

---
+++ test_whenYouAre100.py
@@ -0,0 +1,27 @@
+import pytest
+from hypothesis import given, settings
+import hypothesis.strategies as st
+from hypothesis.database import ExampleDatabase
+from whenYouAre100 import *
+
+@given(st.integers(min_value=0,max_value=101))
+#@settings(database=ExampleDatabase('***'))
+def test_forhundred(i):
+    person = Person('Sharath', i)
+    assert person.whenyouturnhundred() == 2019 + (100-i)
+
+def test_forhundred():
+    person = Person('Sha',100)
+    assert person.whenyouturnhundred() == 2019
+
+def test_forzero():
+    person = Person('Sha',0)
+    assert person.whenyouturnhundred() == 2119
+
+def test_forgreaterthanhundred():
+    person = Person('Sha',101)
+    assert person.whenyouturnhundred() == 2018
+
+def test_somenumber():
+    person = Person('Sha',50)
+    assert person.whenyouturnhundred() == 2069
